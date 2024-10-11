a67691dfaf8f40d49a9a5b597632e75282cb2cf2
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-10-12 00:10:55 +0530

    enable staging CD

--- validator.sh
+++ validator.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Get the filename from the first argument
+
 ENV_VARS_FILE="env-vars.txt"
 # Check if the file exists
 if [[ ! -f "$ENV_VARS_FILE" ]]; then
   echo "File $ENV_VARS_FILE does not exist."
   exit 1
 fi
+
 # Read the file and iterate through each variable name
 while IFS= read -r var_name; do
   # Check if the variable is set in the environment
