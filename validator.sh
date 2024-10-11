d54522e52c1127a0f8703c41c0ba7cd157dd34b0
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-10-11 16:36:19 +0530

    test validator.sh for env var check

---
+++ validator.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read_file()
+{
+  invalid=()
+  file="$FILEPATH"
+  if [ ! -f "$file" ]; then
+    echo "ERROR! $file file does not exist."
+    exit 1
+  fi
+  while IFS="=" read -r key; do
+    [ -z "${{ secrets.key }}" ] && invalid+=("$key")
+  done < "$file"
+  if (( ${#invalid[@]} )); then
+    echo "ERROR! There are some invalid environment variables:"
+    printf '%s\n' "${invalid[@]}"
+    exit 1
+  fi
+}
+
+read_file
