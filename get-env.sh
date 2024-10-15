49da61c53d231523a93082e95bbadeca162238fc
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-10-16 00:19:59 +0530

    fix err

---
+++ get-env.sh
@@ -0,0 +1,7 @@
+# Read file and store each variable name in a space-separated string
+ENV_VAR_NAMES=$(cat environment-variables.txt | tr '\n' ' ')
+echo "env_var_names=$ENV_VAR_NAMES" >> $GITHUB_ENV
+# Loop through the variable names from the file
+for var_name in ${{ env.env_var_names }}; do
+# Use eval to dynamically inject secret
+echo $var_name
