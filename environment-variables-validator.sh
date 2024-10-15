3bcb01f91a95b2f54f9389aaad68f0cc8391a710
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-10-16 00:46:32 +0530

    remove dead code

--- environment-variables-validator.sh
+++ environment-variables-validator.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# ENV_VARS_FILE="environment-variables.txt"
-# # Check if the file exists
-# if [[ ! -f "$ENV_VARS_FILE" ]]; then
-#   echo "File $ENV_VARS_FILE does not exist."
-#   exit 1
-# fi
-
-# Read the file and iterate through each variable name
 for var_name in $ENV_VAR_NAMES; do
   # Check if the variable is set in the environment
   if [[ -z "${!var_name}" ]]; then
