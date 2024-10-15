46abeb7e8714d087d149fbf56c8604520b00faab
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-10-16 00:22:13 +0530

    fix err

--- get-env.sh
+++ get-env.sh
@@ -2,7 +2,7 @@
 ENV_VAR_NAMES=$(cat environment-variables.txt | tr '\n' ' ')
 echo "env_var_names=$ENV_VAR_NAMES" >> $GITHUB_ENV
 # Loop through the variable names from the file
-for var_name in ${{env_var_names}}; do
+for var_name in $env_var_names; do
 # Use eval to dynamically inject secret
 echo $var_name
 done
