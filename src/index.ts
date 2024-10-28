dbeae00adad5bb61474d0aedc01476ef3fd98094
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-10-29 00:44:04 +0530

    try env with txt in console

--- src/index.ts
+++ src/index.ts
@@ -108,7 +108,7 @@ export default {
       }
     }
     console.log("Hi");
-    console.log(env.CLOUDFLARE_ACCOUNT_ID);
+    console.log("env: "+env.CLOUDFLARE_ACCOUNT_ID);
     return router.handle(request, env, ctx);
   },
 
