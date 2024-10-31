89d1d72611248f3354ca6d0b3a68206960b18518
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-11-01 02:56:46 +0530

    test log

--- config/envVarCheck.ts
+++ config/envVarCheck.ts
@@ -8,6 +8,7 @@ validateEnv();
 export default function validateEnv() {
     console.log(process.env.DISCORD_APPLICATION_ID);
     envKeys.forEach((key) => {
+        console.log(process.env[key]);
         if (!process.env[key]) {
           console.warn("Environment variable ${key} is not set.");
         }
