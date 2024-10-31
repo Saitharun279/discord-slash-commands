1874ba0c6bd3238781abf0d5e4646233f84b3fc2
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-11-01 02:40:00 +0530

    env var check through code

--- config/envVarCheck.ts
+++ config/envVarCheck.ts
@@ -1,23 +1,11 @@
-import { env, envKeys } from "../src/typeDefinitions/default.types";
+import { envKeys } from "../src/constants/variables";
 
-function loadEnv(): Pick<env, 'DISCORD_TOKEN' | 'DISCORD_PUBLIC_KEY' | 'DISCORD_APPLICATION_ID'>{
-    const { DISCORD_TOKEN, DISCORD_PUBLIC_KEY, DISCORD_APPLICATION_ID } = process.env;
-  
-    if (!DISCORD_TOKEN || !DISCORD_PUBLIC_KEY || !DISCORD_APPLICATION_ID) {
-      throw new Error("Missing required environment variables");
-    }
-  
-    return {
-      DISCORD_TOKEN,
-      DISCORD_PUBLIC_KEY,
-      DISCORD_APPLICATION_ID,
-    };
-}
+validateEnv();
 
 export default function validateEnv() {
-    type EnvKeysCheck = typeof envKeys[number];
-    type MissingKeys = Exclude<EnvKeysCheck, keyof env>;
-
-    envKeys = process.env;
-    
+    envKeys.forEach((key) => {
+        if (!process.env[key]) {
+          console.warn("Environment variable ${key} is not set.");
+        }
+      });    
 }
