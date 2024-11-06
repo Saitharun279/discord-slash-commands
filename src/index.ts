f672b0d623ac783815bdc98f304ce3f3ef96b4f9
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-11-07 00:20:24 +0530

    review changes

--- src/index.ts
+++ src/index.ts
@@ -107,12 +107,10 @@ export default {
         return new JSONResponse(response.BAD_SIGNATURE, { status: 401 });
       }
     }
-    const envLoadedFromWorker: env = loadEnv(env, true);
-    return router.handle(request, envLoadedFromWorker, ctx);
+    return router.handle(request, loadEnv(env, true), ctx);
   },
 
   async scheduled(req: Request, env: env, ctx: ExecutionContext) {
-    const envLoadedFromWorker: env = loadEnv(env, true);
-    ctx.waitUntil(send(envLoadedFromWorker));
+    ctx.waitUntil(send(loadEnv(env, true)));
   },
 };
